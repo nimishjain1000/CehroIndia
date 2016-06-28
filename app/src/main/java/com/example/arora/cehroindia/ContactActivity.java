4941a50f9cd4a43d60d2cb864ca2474f37cdf6e5
nimishjain1000 <nimishjain1000@gmail.com>
2016-06-28 13:16:23 +0530

    changed contactActivity

--- app/src/main/java/com/example/arora/cehroindia/ContactActivity.java
+++ app/src/main/java/com/example/arora/cehroindia/ContactActivity.java
@@ -1,19 +1,74 @@
 package com.example.arora.cehroindia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class ContactActivity extends AppCompatActivity {
+import static com.example.arora.cehroindia.R.id.progressBar;
 
+public class ContactActivity extends AppCompatActivity  {
+    private ProgressBar mProgressBar;
+    public WebView webview;
+    private static final String TAG = "Main";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
-        WebView webview = new WebView(this);
+       /* WebView webview = new WebView(this);
         setContentView(webview);
         webview.loadUrl("http://cehroindia.org/contact-us/");
 
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.VISIBLE);
+*/
+
+        this.webview = (WebView) findViewById(R.id.webview);
+
+        WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        Context context = getApplicationContext();
+        progressBar = ProgressDialog.show(Main.this, "Showing ProgressDialog", "Loading...");
+
+        webview.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+              Log.i(TAG, "Processing webview url click...");
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+               Log.i(TAG, "Finished loading URL: " + url);
+                if (progressBar.isShowing()) {
+                    progressBar.dismiss();
+                }
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.e(TAG, "Error: " + description);
+                Toast.makeText(ContactActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage(description);
+                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                })
+                alertDialog.show();
+            }
+        });
+        webview.loadUrl("http://cehroindia.org/contact-us/");
     }
 }
